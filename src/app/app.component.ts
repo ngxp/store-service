bfb6c822c87f082977fb8f9d4079e468eadcca85
joostme <github@joost.io>
2018-06-15 19:37:13 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nss-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'nss';
+}
