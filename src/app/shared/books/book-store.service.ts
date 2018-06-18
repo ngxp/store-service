b4b2334edc2b7b3afead3c45efacc93fe4c5c6ed
joostme <github@joost.io>
2018-06-18 10:10:47 +0200

    fix(ngrx-store-service-sample): Fix wrong import path

--- src/app/shared/books/book-store.service.ts
+++ src/app/shared/books/book-store.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Selector } from 'ngrx-store-service';
-import { StoreServiceClass } from 'projects/ngrx-store-service/src/lib/ngrx-store-service';
+import { Selector, StoreServiceClass } from 'ngrx-store-service';
 import { Observable } from 'rxjs';
 import { Book } from 'src/app/shared/books/book.model';
 import { getBooks } from 'src/app/store/books/books.selectors';
