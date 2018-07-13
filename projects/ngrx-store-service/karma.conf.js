e0a974023b743b8ac1ab3ee16495acff832bb082
Joost Zoellner <joost.zoellner@holisticon.de>
2018-07-13 17:03:16 +0200

    fix: fix config running endlessly in CI

--- projects/ngrx-store-service/karma.conf.js
+++ projects/ngrx-store-service/karma.conf.js
@@ -26,7 +26,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: true
   });
 };
