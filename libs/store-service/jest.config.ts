23a7eba7a56901c475738b4ed990052181497eb5
Joost Zöllner <joost.zoellner@holisticon.de>
2022-10-07 14:39:11 +0200

    chore: update NX & Angular to v14

--- libs/store-service/jest.config.ts
+++ libs/store-service/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-    displayName: 'store-service-sample',
+/* eslint-disable */
+export default {
+    displayName: 'store-service',
     preset: '../../jest.preset.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     globals: {
@@ -8,7 +9,7 @@ module.exports = {
             stringifyContentPathRegex: '\\.(html|svg)$',
         },
     },
-    coverageDirectory: '../../coverage/apps/store-service-sample',
+    coverageDirectory: '../../coverage/libs/store-service',
     transform: {
         '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
     },
